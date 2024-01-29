656fc38e50a3e90cd13651ad7ee7325895c26f6f
rojindayani <rojindayani@gmail.com>
2024-01-28 20:46:38 -0500

    Education Page Fixed

--- client/src/education.jsx
+++ client/src/education.jsx
@@ -1,7 +1,26 @@
 export default function Education() {
     return (
         <>
-            <p>Education.Qualification</p>
+            <div className="project-body">
+            <div className="project-card">
+                <h1>My Education</h1>
+                <div className="project-header">
+                    <h2 className="project-title">- Bachelor Of Computer Engineering Degree Candidate</h2>
+                    
+                </div>
+                <p className ="project-details">Sadjad University Of Technology, Mashhad, Iran</p>
+                <p>2018-Present;
+                Anticipated Completion July 2023</p>
+            </div>
+            <div className="project-card">
+                <div className="project-header">
+                    <h2 className="project-title">- Mathematics And Physics High-School Diploma</h2>
+                    
+                </div>
+                <p className ="project-details">Farhikhtegan High-School, Mashhad, Iran </p>
+                <p>2014-2018</p>
+            </div>
+            </div>
         </>
     );
 }
