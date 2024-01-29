1b1d864f62ebd2eb43cf7b584a8d80ffb8849213
rojindayani <rojindayani@gmail.com>
2024-01-28 21:00:06 -0500

    About Page Fixed

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -7,18 +7,22 @@ import '/src/App.css';
 export default function About() {
     return (
         <>
-            <p className='aboutMe'>
-            A computer engineer, equipped with 14 months of hands-on experience as a java backend developer,
-            several months of hands-on experience as a machine- learning researcher and a current game programming student.
-            </p>
+        <body>
+            <div className="aboutMe">
+                <p>
+                A computer engineer, equipped with 14 months of hands-on experience as a java backend developer,
+                several months of hands-on experience as a machine- learning researcher and a current game programming student.
+                </p>
 
-            <img className='rozhin' src={profilePic} alt='Rozhin Dayyani' />
+                <img className='rozhin' src={profilePic} alt='Rozhin Dayyani' />
 
-            <Link
-                to="route"
-                onClick={(event) => { event.preventDefault(); window.open("src/Resume/RozhinDayyaniResume.pdf"); }}>
-                    Resume
-            </Link>
+                <Link
+                    to="route"
+                    onClick={(event) => { event.preventDefault(); window.open("src/Resume/RozhinDayyaniResume.pdf"); }}>
+                        Resume
+                </Link>
+            </div>
+        </body>
         </>
     );
 }    
