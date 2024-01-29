f4851bcad7dd3404dd74c138f60dde3a98788aeb
rojindayani <rojindayani@gmail.com>
2024-01-28 20:19:46 -0500

    Update Project Page Pictures

--- client/src/project.jsx
+++ client/src/project.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import profilePic from './Projects/project1.png';
 import '/src/App.css';
 
 export default function Project() {
@@ -7,10 +6,10 @@ export default function Project() {
         <>
         <div className="project-body">
             <div className="project-card">
-                <h1>Disconnect Shod !!! Omadi Bego Dobare</h1>
+                <h1>My Projects</h1>
                 <div className="project-header">
                     <h2 className="project-title">1. Divar multi-accounting detection:</h2>
-                    <img className='project-Pic' src={profilePic} alt='Rozhin Dayyani' />
+                    <img className='project-Pic' src='./public/project1.png' alt='Rozhin Dayyani' />
                 </div>
                 <p className ="project-details">Divar is a website for selling secondhand goods and advertising.
                 This project aims to detect multi-accounting and bonus abusers</p>
@@ -23,15 +22,28 @@ export default function Project() {
             </div>
             <div className="project-card">
                 <div className="project-header">
-                    <h2 className="project-title">1. Machine Learning Intern (Part-time)</h2>
-                    <img className='project-Pic' src={profilePic} alt='Rozhin Dayyani' />
+                    <h2 className="project-title">2. Store: Online shopping website </h2>
+                    <img className='project-Pic' src='./public/onlineshop.jpg' alt='Rozhin Dayyani' />
                 </div>
-                <p className ="project-details">Divar is a website for selling secondhand goods and advertising.
-                This project aims to detect multi-accounting and bonus abusers</p>
+                <p className ="project-details"></p>
                 <p className="project-roles">My roles:
                 <ul>
-                    <li>Conducting research into different fraud detection algorithms and approaches</li>
-                    <li>Training machine learning models on behavioral biometrics datasets</li>
+                    <li>Writing APIs as a member of the backend team</li>
+                    <li>Guiding the frontend team through the APIs</li>
+                    <li>Fixing the bugs reported by the frontend team</li>
+                </ul>
+                </p>
+            </div>
+            <div className="project-card">
+                <div className="project-header">
+                    <h2 className="project-title">3. Pet-Vet: Veterinary clinic online services website</h2>
+                    <img className='project-Pic' src='./public/vet.jpg' alt='Rozhin Dayyani' />
+                </div>
+                <p className ="project-details"></p>
+                <p className="project-roles">My roles:
+                <ul>
+                    <li>Analysing, designing and developing a microservice named “Comment”</li>
+                    <li>Writing APIs and fixing bugs on other microservices than “Comment”</li>
                 </ul>
                 </p>
             </div>
